cc71d19d5bc60d731b0fcc2da9827d09aa1533bc
velasqueztmonica <velasqueztorresmonica@gmail.com>
2022-06-19 11:12:07 -0500

    adding two exercises to the concole

---
+++ Variables/Introduction.js
@@ -0,0 +1,18 @@
+//Introduction
+
+// 1. Data is printed, or logged, to the console, a panel that displays messages, with console.log().
+
+console.log(31)
+console.log(4)
+
+// 2. We can write single-line comments with // and multi-line comments between /* and */
+// There are 7 fundamental data types in JavaScript: strings, numbers, booleans, null, undefined, symbol, and object.
+//    a. Numbers are any number without quotes: 23.8879
+//    b. Strings are characters wrapped in single or double quotes: 'Sample String'
+//    c. The built-in arithmetic operators include +, -, *, /, and %.
+//    d. Objects, including instances of data types, can have properties, stored information. The properties are denoted with a . after the name of the object, for example: 'Hello'.length.
+//    e. Objects, including instances of data types, can have methods which perform actions. Methods are called by appending the object or instance with a period, the method name, and parentheses. For example: 'hello'.toUpperCase().
+//    f. We can access properties and methods by using the ., dot operator.
+//  g. Built -in objects, including Math, are collections of methods and properties that JavaScript provides.
+
+
