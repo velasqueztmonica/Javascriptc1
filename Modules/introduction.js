d71e63a46e03c6eba33e13520a8e45a6bd3b2c0a
velasqueztmonica <velasqueztorresmonica@gmail.com>
2022-07-08 13:37:01 +0100

    more practice included

---
+++ Modules/introduction.js
@@ -0,0 +1,5 @@
+/*
+
+The answer is that you are executing this code in the browser’s runtime environment. 
+The window.alert() method is built into this environment and any program executed in a browser has access to this method. 
+In fact, the window object provides access to a huge amount of data and functionality relating to the open browser window beyond just .alert().
